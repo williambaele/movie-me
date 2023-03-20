c886700233e3055bc44e5021d512eb5a4d80c74e
William Baele <baelewilliam1999@gmail.com>
2023-03-20 13:15:01 +0100

    Style fixed

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
-console.log("Hello");
-let search = "alibi.com";
-let input = document.querySelector('#input');
-
-function getMovieData(search) {
-  const url = 'https://www.omdbapi.com/?t='+ search + '&apikey=2ecba00f';
-  fetch(url, {
-
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else if (response.status === 404) {
-      throw new Error("Problem with the API");
-    } else {
-      throw new Error("Request failed");
-    }
-  })
-  .then(data => {
-    // Do something with the user data
-    console.log(data);
-    document.querySelector("#movietitle").textContent = data.Title
-    document.querySelector("#movieposter").src = data.Poster
-
-  })
-  .catch(error => {
-    console.log('Error:', error);
-    if (error.message === "Problem with the API") {
-      // Handle the API error
-    }
-  });
+function searchMovie() {
+  const title = document.getElementById("input").value;
+  fetch(`https://www.omdbapi.com/?t=${title}&apikey=2ecba00f`)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("movieInfo").innerHTML = `
+        <h2>${data.Title}</h2>
+        <p>Year: ${data.Year}</p>
+        <p>Director: ${data.Director}</p>
+        <p>Plot: ${data.Plot}</p>
+        <button class="absolute top-0 right-0" onclick="saveMovie()">Save</button>
+      `;
+      // Save the movie data to local storage
+      localStorage.setItem("movie", JSON.stringify(data));
+      console.log(data);
+    })
+    .catch(error => console.log(error));
 }
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   if(inputValue == ""){
+//      document.querySelector("#errormsg").textContent = "Error";
+//      console.log("Empty value");
+//      document.querySelector("#searchbtn").classList.add('cursor-not-allowed');
+//   }
+//   else {
+//     let search = inputValue;
+//     getMovieData(search);
+//   }
+// });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  if(input.value === null ){
-     // Mettre notre bordure de formulaire en rouge (red)
-     document.querySelector("#errormsg").textContent = "Error";
-     document.querySelector("#searchbtn").classList.add('bg-red-400');
+function saveMovie() {
+  const movieData = JSON.parse(localStorage.getItem("movie"));
+  if (movieData) {
+    // Retrieve the existing watch list from local storage
+    const watchList = JSON.parse(localStorage.getItem("watchList")) || [];
+    // Add the movie data to the watch list
+    watchList.push(movieData);
+    // Save the updated watch list to local storage
+    localStorage.setItem("watchList", JSON.stringify(watchList));
+    console.log("Movie saved");
   }
-  else {
-    let search = input.value;
-    getMovieData(search);
-  }
-});
+}

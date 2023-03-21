ffcec5a98483bffe942037368d18981f067626db
William Baele <baelewilliam1999@gmail.com>
2023-03-21 12:18:21 +0100

    Added trash btn to watched movie

--- app.js
+++ app.js
@@ -102,9 +102,9 @@ function displayWatchList(watchList, watchedList = []) {
                   </div>
                 </div>
                 <div>
-                  <label class="flex items-center">
+                  <label class="flex items-center justify-start">
                     <a onclick="deleteWatchedMovie('${movie.imdbID}')" id="trashmovie" class="cursor-pointer">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#E50914" class="bi bi-trash" viewBox="0 0 16 16">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#E50914" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                       </svg>
@@ -185,8 +185,11 @@ function hideLoadWlBtn() {
 }
 
 
-function deleteWatchedMovie() {
-  trashBtn = document.querySelector("#trashmovie");
-  console.log("hi");
+function deleteWatchedMovie(index) {
+  const watchList = JSON.parse(localStorage.getItem("watchList")) || [];
+
   const watchedList = JSON.parse(localStorage.getItem("watchedList")) || [];
+  watchedList.splice(index, 1);
+  localStorage.setItem("watchedList", JSON.stringify(watchedList));
+  displayWatchList(watchList, watchedList);
 }
